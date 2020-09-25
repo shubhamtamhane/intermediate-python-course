db8473dcd203372bbc3dff4ea50df0f34d09fe1c
shubhamtamhane <shubhamtamhane2000@gmail.com>
2020-09-25 19:05:30 +0530

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,12 @@
 import random
 def main():
   roll  = random.randint(1,6)
-  print(f'You rolled a {roll}')
+  if roll == 1:
+    print(f'You rolled a {roll}! Critical Fail')
+  elif roll == 6:
+    print(f'You rolled a {roll}! Critical Success!')
+  else:
+    print(f'You rolled a {roll}')
 
 if __name__== "__main__":
   main()
